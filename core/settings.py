d745b1bc6129a32a05f11ecb7799a8c7c95f2862
begiostonboyev <ostonboyevbegzod@gmail.com>
2025-11-26 10:51:04 +0500

    change settings to hosting

--- core/settings.py
+++ core/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--1o1c)bu99_honxgbxenknawo=$4l&17s++4z=3d@8x)2n-#xh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
